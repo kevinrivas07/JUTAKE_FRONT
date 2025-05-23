c97942e1ba7cc4e085d734284a325e0fc85cae3f
Tatiana <tatianamontoya0428@gmail.com>
2025-05-23 00:39:18 -0500

    se sube front con funcionando

--- src/components/ModifyStatus.jsx
+++ src/components/ModifyStatus.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/ModifyStatus.css';
+import '../Styles/ModifyStatus.css';
 
 const ModifyStatus = () => {
   const [form, setForm] = useState({
